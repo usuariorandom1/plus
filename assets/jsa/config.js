42af7e0e0553a713fb31e4c16f87d71dddca446f
usuarioRandom <72815503+usuariorandom1@users.noreply.github.com>
2021-04-05 09:23:37 -0500

    s

---
+++ assets/jsa/config.js
@@ -0,0 +1,5 @@
+var AppConfig = {
+  "contractAddress": "TDy6VgP4zuwy6ddniXuXYntcNTrkcjw7VC",
+  "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
+  "fullHost": "https://api.shasta.trongrid.io"
+}
