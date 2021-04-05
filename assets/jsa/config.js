7a5bcf301641f80210e7beda7b034edd4f7ce236
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-04-05 15:47:11 -0500

    Update config.js

--- assets/jsa/config.js
+++ assets/jsa/config.js
@@ -1,5 +1,5 @@
 var AppConfig = {
-  "contractAddress": "TPhLYF153q5kDcpTbUDRdRhqScnxZxh5bd",
+  "contractAddress": "TJWbs9pNW63nzZk5CXDhP1YtGseCkVGjNM",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.shasta.trongrid.io"
 }
