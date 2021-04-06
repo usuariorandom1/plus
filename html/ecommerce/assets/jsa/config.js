b22e51c2b4201a891a89f330198ad4287e6d8f43
usuarioRandom <72815503+usuariorandom1@users.noreply.github.com>
2021-04-05 20:19:13 -0500

    1

---
+++ html/ecommerce/assets/jsa/config.js
@@ -0,0 +1,5 @@
+var AppConfig = {
+  "contractAddress": "TTXSRYGRsgyGWAi5jcFV8YSeoj8urPuker",
+  "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
+  "fullHost": "https://api.nileex.io"
+}
