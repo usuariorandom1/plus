73e6987b168a41362781ffd3354e75c6c30dd81d
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-04-06 18:45:32 -0500

    Update app.js

--- assets/jsa/app.js
+++ assets/jsa/app.js
@@ -348,9 +348,9 @@ App = {
       //   }
       // });
       
-      var location = window.location.hostname+'/html/ecommerce/bank_b.html?ref='+addresact;
+      var location = window.location.hostname+'/html/ecommerce/bank_a.html?ref='+addresact;
       $("#Referral").text(location);
-      var location = window.location.hostname+'/html/ecommerce/bank_b.html?ref='+addresact;
+      var location = window.location.hostname+'/html/ecommerce/bank_a.html?ref='+addresact;
       $("#ReferralB").text(location);
     }
     setInterval(refrescar, 2000);
