2a2619062ebc79dfb60291f5884ecfe6055a25c5
usuariorandom1 <72815503+usuariorandom11@users.noreply.github.com>
2021-04-06 17:23:41 -0500

    1

--- assets/jsa/config.js
+++ assets/jsa/config.js
@@ -1,5 +1,5 @@
 var AppConfig = {
-  "contractAddress": "TYdm6RDtwTMqvNSdTwjh2gAg9DTrTTwA3n",
+  "contractAddress": "TQyJz9gb4SAAAausaQXCWqg5to3iLDFmZJ",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
   "fullHost": "https://api.nileex.io"
 }
