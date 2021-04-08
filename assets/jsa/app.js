698a1fcea001b85def8748655ca90a0e03a95112
usuarioRandom <72815503+usuariorandom1@users.noreply.github.com>
2021-04-08 16:28:50 -0500

    1

--- assets/jsa/app.js
+++ assets/jsa/app.js
@@ -322,7 +322,7 @@ App = {
       myContract.subtractTimeB().call().then(timep => {
         this.timepay = parseInt(timep);
         var payuser = this.totalAmountInvB * 2000 / 1000;//* 20 / 1000;
-        payuser = payuser / 300;//86400;
+        payuser = payuser / 600;//86400;
         payuser = (payuser * this.timepay);
         payuser = payuser + this.AVWithdrawB; //Referidos
         
